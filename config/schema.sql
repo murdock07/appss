358d21d4b6360d7a2177f9a5994c4f0449112210
Gabor Nagy <gabor.nagy.2@otpbank.hu>
2018-12-11 13:57:55 +0100

    corrected schema and config ini

--- config/schema.sql
+++ config/schema.sql
@@ -2,15 +2,28 @@ drop table if exists PlantInfos;
 create table PlantInfos (
     PlatnId text,
     PlantName text,
+    MinWaterLevel integer,
+    MaxWaterLevel integer,
+    MinLightLevel integer,
+    MinLightInterval integer,
+    MinTemperature integer,
+    MaxTemperature integer,
+    MinHumidity integer,
+    MaxHumidity integer
 );
-INSERT INTO PlantInfos ()
+INSERT INTO PlantInfos ();
 
 drop table if exists Measurements;
 create table Measurements (
-
+    MeasureTime text,
+    Sensor text,
+    MeasuredValue real,
 );
 
 drop table if exists Logs;
 create table Logs (
-
+    Id integer,
+    LogTime text,
+    LogMessage text,
+    LogSeverity text
 );
