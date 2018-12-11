97934981be25ef2ad7c757801f4f22c2aff959cc
Gabor Nagy <gabor.nagy.2@otpbank.hu>
2018-12-11 13:22:05 +0100

    fixed hub imports

--- webserver/hub.py
+++ webserver/hub.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-import sys, os, sqlite3
+import sys, os, sqlite3, atexit
 sys.path.append(os.path.curdir)
 
 from RPi import GPIO
 from flask import Flask
 from config import config_handler as cfg
+from config import db_handler
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
     print "service started"
 
+@atexit.register
 def stop():
     GPIO.cleanup()
     print "service stopped"
